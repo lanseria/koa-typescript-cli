4d3de43b014c7ce072a9e41de745f03d9d7bc28d
zhangchao <zhangchao@126.com>
2019-08-06 17:18:09 +0800

    加入实例代码

---
+++ src/app/app.ts
@@ -0,0 +1,27 @@
+import * as Koa from "koa";
+import * as HttpStatus from "http-status-codes";
+import movieController from "../movie/movie.controller";
+
+const app: Koa = new Koa();
+
+// Generic error handling middleware.
+app.use(async (ctx: Koa.Context, next: () => Promise<any>) => {
+  try {
+    await next();
+  } catch (error) {
+    ctx.status =
+      error.statusCode || error.status || HttpStatus.INTERNAL_SERVER_ERROR;
+    error.status = ctx.status;
+    ctx.body = { error };
+    ctx.app.emit("error", error, ctx);
+  }
+});
+
+// Route middleware.
+app.use(movieController.routes());
+app.use(movieController.allowedMethods());
+
+// Application error logging.
+app.on("error", console.error);
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,38 @@
-import * as Koa from "koa";
-import * as Router from "koa-router";
-import * as logger from "koa-logger";
-import * as json from "koa-json";
-import * as bodyParser from "koa-bodyparser";
+import app from "./app/app";
 
-const app = new Koa();
-const router = new Router();
+// Process.env will always be comprised of strings, so we typecast the port to a
+// number.
+const PORT: number = Number(process.env.PORT) || 3000;
 
-interface HelloRequest {
-  name: String;
-}
-
-router.post("/", async (ctx, next) => {
-  const data = <HelloRequest>ctx.request.body;
-  ctx.body = { name: data.name };
-  await next();
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
 
-app.use(json());
-app.use(logger());
-app.use(bodyParser());
+// import * as Koa from "koa";
+// import * as Router from "koa-router";
+// import * as logger from "koa-logger";
+// import * as json from "koa-json";
+// import * as bodyParser from "koa-bodyparser";
 
-app.use(router.routes()).use(router.allowedMethods());
+// const app = new Koa();
+// const router = new Router();
 
-app.listen(3000, () => {
-  console.log("Server running on port http://localhost:3000");
-});
+// interface HelloRequest {
+//   name: String;
+// }
+
+// router.post("/", async (ctx, next) => {
+//   const data = <HelloRequest>ctx.request.body;
+//   ctx.body = { name: data.name };
+//   await next();
+// });
+
+// app.use(json());
+// app.use(logger());
+// app.use(bodyParser());
+
+// app.use(router.routes()).use(router.allowedMethods());
+
+// app.listen(3000, () => {
+//   console.log("Server running on port http://localhost:3000");
+// });
