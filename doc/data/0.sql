57efc59634fc548adf08b137d509b945cdc79ab3
zhangchao <zhangchao@126.com>
2019-11-14 17:32:42 +0800

    添加基础数据

---
+++ doc/data/0.sql
@@ -0,0 +1,12 @@
+INSERT INTO "public"."role" ("id", "name", "identification", "value", "info") VALUES ('31ce56d5-5e07-4e2f-8d30-238570eca244', '系统权限', 'SYS_SETTING', '0,0', '系统权限');
+INSERT INTO "public"."role" ("id", "name", "identification", "value", "info") VALUES ('17cfda01-4b2b-4f78-b32a-dd4af3b4cb9d', '数据库权限', 'DATA_ADMIN', '0,8', '数据库权限');
+INSERT INTO "public"."role" ("id", "name", "identification", "value", "info") VALUES ('ac781a09-0d6b-4f93-bb4a-6bb408685426', '用户新增权限', 'USER_ADD', '0,22', '用户新增权限');
+INSERT INTO "public"."role" ("id", "name", "identification", "value", "info") VALUES ('32e48dc7-e28c-46a3-ac6c-e7d3346bf15e', '用户编辑权限', 'USER_EDIT', '0,30', '用户编辑权限');
+INSERT INTO "public"."role" ("id", "name", "identification", "value", "info") VALUES ('7722359a-c90b-4c8e-ae67-1e7061ed6b42', '用户查看权限', 'USER_VIEW', '1,2', '用户查看权限');
+INSERT INTO "public"."role" ("id", "name", "identification", "value", "info") VALUES ('fef99317-73f1-4e0d-8faf-9b23ab85f221', '用户删除权限', 'USER_DELETE', '1,17', '用户删除权限');
+INSERT INTO "public"."role" ("id", "name", "identification", "value", "info") VALUES ('53ed52af-7601-4aa8-b605-4c86119f5c92', '文章新增权限', 'POST_ADD', '1,28', '文章新增权限');
+INSERT INTO "public"."role" ("id", "name", "identification", "value", "info") VALUES ('f195cc62-7305-464a-93a1-f64fd521497e', '文章编辑权限', 'POST_EDIT', '2,4', '文章编辑权限');
+INSERT INTO "public"."role" ("id", "name", "identification", "value", "info") VALUES ('df374417-9f86-4fd7-aae8-e7d45ea1027b', '文章查看权限', 'POST_VIEW', '2,19', '文章查看权限');
+INSERT INTO "public"."role" ("id", "name", "identification", "value", "info") VALUES ('8ed5341a-dedb-4e24-b3e9-53866e89c9e4', '文章删除权限', 'POST_DELETE', '2,25', '文章删除权限');
+
+INSERT INTO "public"."user" ("id", "username", "password", "age") VALUES ('3abf2d65-69ec-4c3a-a9d8-4993fe6d2659', 'root', '123456', '15');
